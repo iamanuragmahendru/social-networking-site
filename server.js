8e3339c895ceb7fdb5dbc4ffba308cb46c501cef
Anurag Mahendru <iamanuragmahendru@gmail.com>
2019-05-24 02:17:19 +0530

    Added folders and files

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json())
+app.use(express.urlencoded({exxtended:true}))
+
+const userRoute = require('./routes/user')
+const adminRoute = require('./routes/admin')
+
+app.use(express.static('public'))
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
