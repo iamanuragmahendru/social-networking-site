8d13046a51bed4e0fa5158245f45a0b1582e086f
Anurag Mahendru <iamanuragmahendru@gmail.com>
2019-06-26 01:39:43 +0530

    Changes for Heroku

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const http = require('http')
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
-const PORT = process.env.port || 3000
+const PORT = process.env.PORT || 3000
 
 // Defining the various routes to handle traffic
 
